594efb6d5a7aa765d77bcd11639e21c36f83e5b7
michael.schechner <242@holbertonschool.com>
2018-01-18 07:58:44 +0000

    fix 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - uses system call to print a string to stderr
+ * Return: returns 1 on termination
+ */
 
-int main(){
-        write( 2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 60);
-	return 1;
+int main(void)
+{
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n"
+, 60);
+	return (1);
 }
