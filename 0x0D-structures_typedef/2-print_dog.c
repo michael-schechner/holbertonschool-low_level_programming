a754a081e8a32f32e9793e4b5653f20b8d13c2bc
michael-schechner <242@holbertonschool.com>
2018-02-20 07:44:14 +0000

    u/l files

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+  if(d != NULL)
+    {
+      printf("Name: %s\n", ((d->name == NULL) ? "(nil)" : d->name));
+      printf("Age: %f\n", d->age);
+      printf("Owner: %s\n", ((d->owner == NULL) ? "(nil)" : d->owner));
+    }
+}
---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  struct dog *new_dog;
+
+  new_dog = malloc(sizeof(struct dog));
+  if (new_dog == NULL)
+    {
+      return (NULL);
+    }
+  new_dog->name = name;
+  new_dog->age = age;
+  new_dog->owner = owner;
+  return (new_dog);
+
+}
